79d3801bbe6b3db7efa9aaff4e31c13cbaa6b7d6
17rishu <17singhrishabh@gmail.com>
2024-12-02 02:07:36 +0530

    githubInfo API

--- index.js
+++ index.js
@@ -15,6 +15,46 @@ app.get('/login', (req, res)=>{
     res.send('<h2> Abhi login page ni bna, kahi aur ja ke login kr le :(')
 })
 
+const githubInfo = {
+  login: "17rishu",
+  id: 73096439,
+  node_id: "MDQ6VXNlcjczMDk2NDM5",
+  avatar_url: "https://avatars.githubusercontent.com/u/73096439?v=4",
+  gravatar_id: "",
+  url: "https://api.github.com/users/17rishu",
+  html_url: "https://github.com/17rishu",
+  followers_url: "https://api.github.com/users/17rishu/followers",
+  following_url: "https://api.github.com/users/17rishu/following{/other_user}",
+  gists_url: "https://api.github.com/users/17rishu/gists{/gist_id}",
+  starred_url: "https://api.github.com/users/17rishu/starred{/owner}{/repo}",
+  subscriptions_url: "https://api.github.com/users/17rishu/subscriptions",
+  organizations_url: "https://api.github.com/users/17rishu/orgs",
+  repos_url: "https://api.github.com/users/17rishu/repos",
+  events_url: "https://api.github.com/users/17rishu/events{/privacy}",
+  received_events_url: "https://api.github.com/users/17rishu/received_events",
+  type: "User",
+  user_view_type: "public",
+  site_admin: false,
+  name: "Rishabh Singh",
+  company: null,
+  blog: "",
+  location: null,
+  email: null,
+  hireable: null,
+  bio: "Software Engineer\r\n",
+  twitter_username: null,
+  public_repos: 22,
+  public_gists: 0,
+  followers: 0,
+  following: 0,
+  created_at: "2020-10-19T06:18:22Z",
+  updated_at: "2024-12-01T20:20:11Z",
+};
+
+app.get('/github', (req, res)=>{
+    res.json(githubInfo)
+})
+
 app.listen(process.env.PORT, ()=>{
     console.log(`Server is running on port ${port}`)
 })
